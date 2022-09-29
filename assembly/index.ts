920df8b3cba0022a8292f20df0fe5ae6b81a57dc
hussam-aldarwish <hussam.aldarwish@hotmail.com>
2022-09-29 05:57:28 +0300

    C - Create

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,9 @@
 // The entry file of your WebAssembly module.
+import { Todo } from "./model";
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
+// export the create method. This acts like an endpoint
+// that we'll be able to call from our web app.
+export function create(task: string): Todo {
+  // use the Todo class to persist the todo data
+  return Todo.insert(task);
 }
--- assembly/model.ts
+++ assembly/model.ts
@@ -19,4 +19,17 @@ export class Todo {
     this.task = task;
     this.done = false;
   }
+
+  static insert(task: string): Todo {
+    // create a new Todo
+    const todo = new Todo(task);
+
+    // add the todo to the PersistentUnorderedMap
+    // where the key is the todo's id and the value
+    // is the todo itself. Think of this like an
+    // INSERT statement in SQL.
+    todos.set(todo.id, todo);
+
+    return todo;
+  }
 }
